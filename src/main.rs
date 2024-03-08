6a5b3e9e5b4ea5352564fc71f85d5b7a80a30c43
a6.webm <46289515+a6-webm@users.noreply.github.com>
2024-03-08 17:44:03 +0000

    working external theremin

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,13 @@
 
 use dioxus::prelude::*;
 use dioxus_desktop::{Config, WindowBuilder};
-use std::{thread, time::Duration};
-use theramin::use_watch_routine::*;
+use std::io;
+use theramin::{
+    input::InputHandler,
+    manymouse::{self, Axis, Button, ManyMouse},
+    midi::MidiInitialiser,
+    use_watch_routine::*,
+};
 
 fn main() {
     // simple_logger::init_with_level(log::Level::Debug).unwrap();
@@ -18,58 +23,49 @@ fn main() {
 #[component]
 fn App(cx: Scope) -> Element {
     let rx = use_watch_routine(cx, 0u32, |tx| async move {
-        let mut ext_counter = 0u32;
+        let mut m_mouse = ManyMouse::new();
+        println!("{}", m_mouse.driver_name());
+        println!("{:?}", m_mouse.device_list());
+
+        let mut buffer = String::new();
+        let stdin = io::stdin();
+        print!("Which dev bruh: ");
+        stdin.read_line(&mut buffer).unwrap();
+        let dev_idx: u32 = buffer.trim().parse().unwrap();
+
+        let mut midi_h = MidiInitialiser::new().virtual_port("port_1");
+
+        let mut input_h = InputHandler::new(50);
+
         loop {
-            thread::sleep(Duration::from_secs(1));
-            ext_counter += 1;
-            tx.send(ext_counter).unwrap();
+            for ev in m_mouse.poll() {
+                println!("{:?}", ev);
+                if ev.device != dev_idx {
+                    continue;
+                }
+                match ev.ev_type {
+                    manymouse::EventType::Relmotion if ev.item == Button::LMB as u32 => {
+                        let pitch = input_h.handle_rel_move(ev.value);
+                        if input_h.playing {
+                            midi_h.play(pitch);
+                        }
+                        tx.send(input_h.pos()).unwrap();
+                    }
+                    manymouse::EventType::Button if ev.item == Axis::X as u32 => {
+                        input_h.playing = ev.value == 1;
+                        if input_h.playing {
+                            midi_h.play(input_h.pitch_from_pos());
+                        } else {
+                            midi_h.release();
+                        }
+                    }
+                    _ => (),
+                }
+            }
         }
     });
 
-    let val = rx.read().unwrap().to_owned();
+    let pos = rx.read().unwrap().to_owned();
 
-    cx.render(rsx!("Hello world {val}"))
+    cx.render(rsx!("Hello world {pos}"))
 }
-
-// {
-//             let mut m_mouse = ManyMouse::new();
-//             println!("{}", m_mouse.driver_name());
-//             println!("{:?}", m_mouse.device_list());
-
-//             let mut buffer = String::new();
-//             let stdin = io::stdin();
-//             print!("Which dev bruh: ");
-//             io::stdout().flush().unwrap();
-//             stdin.read_line(&mut buffer).unwrap();
-//             let dev_idx: u32 = buffer.trim().parse().unwrap();
-
-//             let mut midi_h = MidiInitialiser::new().virtual_port("port_1");
-
-//             let mut input_h = InputHandler::new(50);
-
-//             loop {
-//                 for ev in m_mouse.poll() {
-//                     println!("{:?}", ev);
-//                     if ev.device != dev_idx {
-//                         continue;
-//                     }
-//                     match ev.ev_type {
-//                         manymouse::EventType::Relmotion if ev.item == Button::LMB as u32 => {
-//                             let pitch = input_h.handle_rel_move(ev.value);
-//                             if input_h.playing {
-//                                 midi_h.play(pitch);
-//                             }
-//                         }
-//                         manymouse::EventType::Button if ev.item == Axis::X as u32 => {
-//                             input_h.playing = ev.value == 1;
-//                             if input_h.playing {
-//                                 midi_h.play(input_h.pitch_from_pos());
-//                             } else {
-//                                 midi_h.release();
-//                             }
-//                         }
-//                         _ => (),
-//                     }
-//                 }
-//             }
-//         }
