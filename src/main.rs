090666d61fb45c6998889d67883c6f1f1983baf6
a6.webm <46289515+a6-webm@users.noreply.github.com>
2024-03-30 16:35:44 +0000

    cursor grabbing working

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 #![allow(non_snake_case)]
 
 use dioxus::prelude::*;
-use dioxus_desktop::{Config, LogicalSize, WindowBuilder};
+use dioxus_desktop::{
+    tao::keyboard::KeyCode, use_window, use_wry_event_handler, Config, LogicalSize, WindowBuilder,
+};
 
 use theramin::{midi::HIGHEST_MIDI_NOTE, use_theramin_routine::*};
 
@@ -10,13 +12,29 @@ fn main() {
         App,
         vec![],
         Config::default()
-            .with_window(WindowBuilder::new().with_inner_size(LogicalSize::new(400.0, 800.0))),
+            .with_window(WindowBuilder::new().with_inner_size(LogicalSize::new(400.0, 800.0)))
+            .with_menu(None)
+            .with_disable_context_menu(true),
     );
+}
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+enum CursorState {
+    NoGrab,
+    Grab,
+    PendingUnGrab,
+}
+
+impl CursorState {
+    fn is_grabbed(self) -> bool {
+        self != CursorState::NoGrab
+    }
 }
 
 #[component]
 fn App() -> Element {
     use_theramin_routine();
+    let cursor_state = use_context_provider(|| Signal::new(CursorState::NoGrab));
     rsx! {
         style {
             "html, body {{
@@ -32,8 +50,46 @@ fn App() -> Element {
             background_color: "#00001a",
             display: "flex",
             flex_direction: "row",
+            if cursor_state.read().is_grabbed() {
+                MouseHider {}
+            },
             DevBar {},
             ThereminList {},
+        }
+    }
+}
+
+#[component]
+fn MouseHider() -> Element {
+    let mut cursor_state: Signal<CursorState> = use_context();
+    let window = use_window();
+    use_effect(move || {
+        window.set_cursor_visible(false);
+        window.set_cursor_grab(true).unwrap();
+    });
+    use dioxus_desktop::tao::event::Event::WindowEvent;
+    use_wry_event_handler(move |ev, _| {
+        if let WindowEvent { event, .. } = ev {
+            use dioxus_desktop::tao::event::WindowEvent;
+            if let WindowEvent::KeyboardInput { event, .. } = event {
+                if event.physical_key == KeyCode::Escape {
+                    *cursor_state.write() = CursorState::PendingUnGrab;
+                }
+            }
+        }
+    });
+    let window = use_window();
+    if *cursor_state.read() == CursorState::PendingUnGrab {
+        window.set_cursor_visible(true);
+        window.set_cursor_grab(false).unwrap();
+        *cursor_state.write() = CursorState::NoGrab;
+    }
+    rsx! {
+        div {
+            position: "absolute",
+            padding: "50vh 50vw",
+            top: "0",
+            left: "0",
         }
     }
 }
@@ -95,14 +151,28 @@ fn DevList() -> Element {
 
 #[component]
 fn ThereminList() -> Element {
-    println!("rendered ThereminList");
     let devices: Signal<Devices> = use_context();
     let theremin_positions = use_theremin_positions();
+    let mut cursor_state: Signal<CursorState> = use_context();
     rsx! {
         div {
             flex: "1 1 100%",
             min_width: "0",
             border: "solid white",
+            button {
+                "type": "button",
+                width: "100%",
+                display: "block",
+                disabled: cursor_state.read().is_grabbed(),
+                onclick: move |_| {
+                    *cursor_state.write() = CursorState::Grab;
+                },
+                if cursor_state.read().is_grabbed() {
+                    "Press Esc to release cursor"
+                } else {
+                    "Capture cursor"
+                }
+            },
             for (dev, pos) in devices
                 .iter()
                 .filter(|d| d.selected)
@@ -124,7 +194,6 @@ fn ThereminList() -> Element {
 
 #[component]
 fn NoteBar(note_width: f32, note_scroll: f32) -> Element {
-    println!("rendered NoteBar");
     let offset = 50.0 - note_scroll * note_width;
     rsx! {
         div {
